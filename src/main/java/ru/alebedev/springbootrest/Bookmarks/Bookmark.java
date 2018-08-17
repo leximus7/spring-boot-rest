1bba7d723ea832af2583e0aaff26d3c07a01cb3b
Alexey Lebedev <aalebedev@sberbank.ru>
2018-08-17 12:34:15 +0300

    initial commit

---
+++ src/main/java/ru/alebedev/springbootrest/Bookmarks/Bookmark.java
@@ -0,0 +1,54 @@
+package ru.alebedev.springbootrest.Bookmarks;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Bookmark {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @JsonIgnore
+    @ManyToOne
+    private Account account;
+
+    private String uri;
+
+    private String description;
+
+    public Bookmark() {
+    }
+
+    public Bookmark(final Account account, final String uri, final String description) {
+        this.account = account;
+        this.uri = uri;
+        this.description = description;
+    }
+
+    public static Bookmark from(Account account, Bookmark bookmark) {
+        return new Bookmark(account, bookmark.uri, bookmark.getDescription());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ src/main/java/ru/alebedev/springbootrest/Bookmarks/BookmarkRepository.java
@@ -0,0 +1,10 @@
+package ru.alebedev.springbootrest.Bookmarks;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+    Collection<Account> findByAccountUsername(String username);
+}
